0b70f2d6f96e6fc8d57f61003f683f1826977525
Cong Meng <samuelmc1015@gmail.com>
2019-09-16 12:29:36 -0400

    lockpick animation

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-$(function(){
+$(function () {
     mode = {
         'Easy Mode': 'Hard Mode',
         'Hard Mode': 'Easy Mode',
     }
 
-    $('#level-switch').on('click', function (evt){
+    $('#level-switch').on('click', function (evt) {
         $('#level-label').html(mode[$('#level-label').html()]);
+    });
+
+    $(document).keypress(function (evt) {
+        switch (evt.which) {
+            // left
+            case 100:
+                $("#pick").animate({ left: "+=5" }, 1);
+                break;
+
+            // right
+            case 97:
+                $("#pick").animate({ left: "-=5" }, 1);
+                break;
+
+            // up
+            case 119:
+                $("#pick").animate({ top: "-=5" }, 1);
+                break;
+
+            // down
+            case 115:
+                $("#pick").animate({ top: "+=5" }, 1);
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        evt.preventDefault(); // prevent the default action (scroll / move caret)
     });
 });
