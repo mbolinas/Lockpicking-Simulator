33bafe3b6786fc5580b81b06bea59196e1b7d8e4
Marc Bolinas <bolinas.marc@gmail.com>
2019-09-18 16:35:08 -0400

    bunch of cleanup

--- index.js
+++ index.js
@@ -6,43 +6,53 @@ $(function(){
 
     //TODO: convert to pin_array.push(randomized_amt)
     var pin_array = [300,500,600];
+    var pin_array_init = [300, 500, 600];  //so that reset works
     var pin_falltime = [1000,1000,1000];
     var pin_set_range = [2000,2000,2000];
-    var selected_pin = -1;
 
+
+    var selected_pin = -1;
     var time = 0;
 
-    var str = '';
+    function randomize(){
+
+    }
+
+    function reset(){
+        selected_pin = -1;
+        time = 0;
+        update();
+    }
 
     function keydownHandler(e){
         switch(e.code){
             case 'KeyA':
-                selected_pin = Math.max(0, selected_pin - 1);
-                time = 0;
-                update();
+                //if statement prevents you from changing pin while a pin is falling
+                if(Date.now() - time > pin_max_fall_time_ms){
+                    selected_pin = Math.max(0, selected_pin - 1);
+                    update();
+                }
                 break;
             case 'KeyD':
-                selected_pin = Math.min(pin_count - 1, selected_pin + 1);
-                time = 0;
-                update();
+                if(Date.now() - time > pin_max_fall_time_ms){
+                    selected_pin = Math.min(pin_count - 1, selected_pin + 1);
+                    update();
+                }
                 break;
             case 'Space':
+                //this means this pin is already set
                 if(pin_array[selected_pin] === -1){
-                    //this means this pin is already set
                     //we do nothing, probably just animate the lockpick kicking an empty chamber
                 }
+                //if time between presses is greater than it takes for the pin to fall down, push pin up
                 else if(Date.now() - time > pin_max_fall_time_ms){
-
-                    
-                    //if time between presses is greater than it takes for the pin to fall down, push pin up
                     time = Date.now();
                     $('#pin' + (selected_pin)).css({top: '0px', transition: 'top .15s'});
-                    str = 'top ' + (pin_falltime[selected_pin] / 1000) + 's';
-                    //TODO: if the user switches pins before setTimeout fires, it will call the wrong pin
                     setTimeout(() => {
-                        $('#pin' + (selected_pin)).css({top: '200px', transition: str});
+                        $('#pin' + (selected_pin)).css({top: '200px', transition: 'top ' + (pin_falltime[selected_pin] / 1000) + 's'});
                     }, 200);
                 }
+                //the pin is currently falling, user is trying to set it
                 else{
                     //TODO: convert 2000 to a random amount per pin, found in pin_set_range
                     if(Math.abs(Date.now() - time - pin_array[selected_pin]) < 2000){
