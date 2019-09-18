0e68cd3c093d70aabdb3e1dea1149389a1eda593
Marc Bolinas <bolinas.marc@gmail.com>
2019-09-18 13:29:59 -0400

    chamber spawning now in js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $(function(){
+
+    const pin_count = 3;
+
     mode = {
         'Easy Mode': 'Hard Mode',
         'Hard Mode': 'Easy Mode',
@@ -7,4 +10,10 @@ $(function(){
     $('#level-switch').on('click', function (evt){
         $('#level-label').html(mode[$('#level-label').html()]);
     });
+    
+    for(i = 0; i < pin_count; i++){
+        var chamber = '<div id=chamber' + i + ' class=chamber></div>';
+        $('.lockpick-container').append(chamber);
+        $('#chamber' + i).css({top: '0px', left: '0px'});
+    }
 });
