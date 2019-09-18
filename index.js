9c59a16acfe7bf708ff78e7ba5722e661336f9be
Marc Bolinas <bolinas.marc@gmail.com>
2019-09-18 15:50:55 -0400

    cleanup

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ $(function(){
     var pin_array = [300,500,600];
     var pin_falltime = [1000,1000,1000];
     var pin_set_range = [2000,2000,2000];
-    var selected_pin = 0;
+    var selected_pin = -1;
 
     var time = 0;
 
     var str = '';
 
     function keydownHandler(e){
-
-        //alert(e.code);
-
         switch(e.code){
             case 'KeyA':
                 selected_pin = Math.max(0, selected_pin - 1);
@@ -25,37 +22,31 @@ $(function(){
                 update();
                 break;
             case 'KeyD':
-                selected_pin = Math.min(pin_count, selected_pin + 1);
+                selected_pin = Math.min(pin_count - 1, selected_pin + 1);
                 time = 0;
                 update();
                 break;
             case 'Space':
-                //$('.test').css({left: '100vw', transition: 'left 5s'});
-                if(pin_array[selected_pin - 1] === -1){
+                if(pin_array[selected_pin] === -1){
                     //this means this pin is already set
-
-                    //we do nothing
-                    //alert('bruh');
+                    //we do nothing, probably just animate the lockpick kicking an empty chamber
                 }
                 else if(Date.now() - time > pin_max_fall_time_ms){
 
                     
                     //if time between presses is greater than it takes for the pin to fall down, push pin up
                     time = Date.now();
-                    $('#pin' + (selected_pin - 1)).css({top: '0px', transition: 'top .15s'});
-                    str = 'top ' + (pin_falltime[selected_pin - 1] / 1000) + 's';
+                    $('#pin' + (selected_pin)).css({top: '0px', transition: 'top .15s'});
+                    str = 'top ' + (pin_falltime[selected_pin] / 1000) + 's';
                     //TODO: if the user switches pins before setTimeout fires, it will call the wrong pin
                     setTimeout(() => {
-                        $('#pin' + (selected_pin - 1)).css({top: '200px', transition: str});
+                        $('#pin' + (selected_pin)).css({top: '200px', transition: str});
                     }, 200);
-                    //$('#chamber' + (selected_pin - 1)).css({top: '200px', transition: str});
-                    //alert('kicking');
                 }
                 else{
-                    //alert('hitit');
                     //TODO: convert 200 to a random amount per pin
-                    if(Math.abs(Date.now() - time - pin_array[selected_pin - 1]) < 2000){
-                        pin_array[selected_pin - 1] = -1;
+                    if(Math.abs(Date.now() - time - pin_array[selected_pin]) < 2000){
+                        pin_array[selected_pin] = -1;
                     }
                 }
                 update();
@@ -66,7 +57,7 @@ $(function(){
     //make update() handle lockpick positioning onto correct pin
     function update(){
         
-        $('.game-title').text('current pin: ' + selected_pin + ';     time (-1 for done): ' + pin_array[selected_pin - 1] + 'str: ' + str);
+        $('.game-title').text('current pin: ' + selected_pin + ';     time (-1 for done): ' + pin_array[selected_pin] + 'str: ' + str);
     }
 
     mode = {
